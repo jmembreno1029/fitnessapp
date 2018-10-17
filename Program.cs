8b1e7f003f05c4f70f079f76b384b87417009069
Joshua Membreno <membrenojoshua@gmail.com>
2018-10-17 00:36:33 -0500

    moved local project to github repo

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace FitnessApp {
+    class Program {
+        static void Main () {
+            var runningTotal = 0.0;
+
+            while (true) {
+                Console.Write ("Enter how many minutes you exercised or type \"quit\" to exit: ");
+
+                string entry = Console.ReadLine ();
+
+                if (entry.ToLower () == "quit") {
+                    break;
+                }
+
+                try {
+                    var minutes = double.Parse (entry);
+                    if (minutes <= 0) {
+                        Console.WriteLine (minutes + " is not an acceptable value");
+                        continue;
+                    } else if (minutes <= 10) {
+                        Console.WriteLine ("Better than nothing, am I right?");
+                    } else if (minutes <= 30) {
+                        Console.WriteLine ("Way to go hot stuff!");
+                    } else if (minutes <= 60) {
+                        Console.WriteLine ("You must be a ninja warrior in training!");
+                    } else {
+                        Console.WriteLine ("Okay, now you're just showing off!");
+                    }
+
+                    runningTotal += minutes;
+
+                } catch (FormatException) {
+                    Console.WriteLine ("That is not valid input");
+                    continue;
+                }
+
+                Console.WriteLine ("You've exercised " + runningTotal + " minutes.");
+
+            }
+            Console.WriteLine ("Goodbye");
+            Console.ReadKey ();
+
+        }
+    }
+}
